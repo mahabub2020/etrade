c32581bf28c678f1a11df8338229d6d284595bda
mesbah <mesbah@axilweb.com>
2023-08-01 18:21:44 +0600

    cart added

--- assets/application.js
+++ assets/application.js
@@ -1,5 +1,11 @@
-$(document).ready(function(){
+(function ($) {
+    "use strict";
+
     $('.cart-dropdown-btn').on('click', function() {
         $('.cart-dropdown').addClass('open');
     });
-});// Put your application javascript here
+
+    $('.cart-close').on('click', function() {
+        $('.cart-dropdown').removeClass('open');
+    });
+})(jQuery);
