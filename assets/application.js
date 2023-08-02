b7b143582a85722e2f9d7f0e6b4d684777b3ba23
mesbah <mesbah@axilweb.com>
2023-08-02 18:34:24 +0600

    form submit js

--- assets/application.js
+++ assets/application.js
@@ -11,10 +11,10 @@ $(document).ready(function(){
     $('form[action="/cart/add"]').on('submit', async function(e) {
         e.preventDefault();
         
-        // await fetch("cart/add", {
-        //     method: "post",
-        //     body: new FormData(this),
-        // });
+        await fetch("cart/add", {
+            method: "post",
+            body: new FormData(this),
+        });
         const res = await fetch("/cart.json");
         const cart = await res.json();
         $('.cart-count').html(cart.item_count);
