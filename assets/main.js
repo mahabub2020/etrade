0c39697e629499835e8909cda15758285cd751fc
Mahabub Hasan <mhmanik@users.noreply.github.com>
2023-08-03 14:54:12 +0600

    add g js

--- assets/main.js
+++ assets/main.js
@@ -2,7 +2,6 @@
  
     'use strict';
      
-
         $(document).ready(function(){
             $('.categrie-product-activation-4').slick({
                 infinite: true,
@@ -15,7 +14,59 @@
                 prevArrow: '<button class="slide-arrow prev-arrow"><i class="fal fa-angle-left"></i></button>',
                 nextArrow: '<button class="slide-arrow next-arrow"><i class="fal fa-angle-right"></i></button>',
             });
-          });
+ 
+            $('.categrie-product-activation').slick({
+                infinite: true,
+                slidesToShow: 7,
+                slidesToScroll: 7,
+                arrows: true,
+                dots: false,
+                autoplay: false,
+                speed: 1000,
+                prevArrow: '<button class="slide-arrow prev-arrow"><i class="fal fa-long-arrow-left"></i></button>',
+                nextArrow: '<button class="slide-arrow next-arrow"><i class="fal fa-long-arrow-right"></i></button>',
+                responsive: [
+                    {
+                        breakpoint: 1199,
+                        settings: {
+                            slidesToShow: 6,
+                            slidesToScroll: 6
+                        }
+                    },
+                    {
+                        breakpoint: 991,
+                        settings: {
+                            slidesToShow: 4,
+                            slidesToScroll: 4
+                        }
+                    },
+                    {
+                        breakpoint: 767,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 3
+                        }
+                    },
+                    {
+                        breakpoint: 479,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2
+                        }
+                    },
+                    {
+                        breakpoint: 400,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1
+                        }
+                    },
+
+                ]
+            });
+
+
+        });
 
 
 
