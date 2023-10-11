7cf1b5d6b191f98c1a0f78ab121037d9dda85781
mesbah <mesbah@axilweb.com>
2023-10-11 16:50:07 +0600

    shop with sidebar

--- assets/application.js
+++ assets/application.js
@@ -220,3 +220,15 @@ function debounce(fn, wait) {
       t = setTimeout(() => fn.apply(this, args), wait);
     };
 }
+
+function onKeyUpEscape(event) {
+  if (event.code.toUpperCase() !== 'ESCAPE') return;
+
+  const openDetailsElement = event.target.closest('details[open]');
+  if (!openDetailsElement) return;
+
+  const summaryElement = openDetailsElement.querySelector('summary');
+  openDetailsElement.removeAttribute('open');
+  summaryElement.setAttribute('aria-expanded', false);
+  summaryElement.focus();
+}
