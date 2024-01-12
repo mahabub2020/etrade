e66cf45b21262286e2c195e03c7beae3fe878cb2
mhmanik <m.manik01@gmail.com>
2024-01-12 17:17:31 +0600

    updatedec

--- assets/main.js
+++ assets/main.js
@@ -13,12 +13,31 @@
             magnificPopupActivation();
             axilslickactivation3();
             axilSlickActivation();
+            axilBackToTop();
 
         });
 
+    function  axilBackToTop() {
+        var btn = $('#backto-top');
+        $(window).scroll(function () {
+            if ($(window).scrollTop() > 300) {
+                btn.addClass('show');
+            } else {
+                btn.removeClass('show');
+            }
+        });
+        btn.on('click', function (e) {
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: 0
+            }, '300');
+        });
+    }
+    
 document.addEventListener("shopify:section:load", () => {
     axilSlickActivation();
     countdown(); 
+    axilBackToTop();
 
 });
 
